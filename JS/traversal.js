5edb55ec19182be2fe7ed108f417817f5184d5fc
lafayete <wushibao2008@qq.com>
2020-02-09 20:31:01 +0800

    .add traversal

---
+++ JS/traversal.js
@@ -0,0 +1,36 @@
+//DFS 深度优先遍历
+
+const deepTraversal = function(node, nodeList = []) {
+  if (node !== null) {
+    nodeList.push(node);
+    let children = node.children;
+    for (let i = 0; i < children.length; i++) {
+      deepTraversal(children[i], nodeList);
+    }
+  }
+  return nodeList;
+};
+
+//BFS 广度优先遍历
+
+const widthTraversal = function(node) {
+  const stack = [];
+  const nodes = [];
+  if (node) {
+    stack.push(node);
+    while (stack.length) {
+      let item = stack.shift();
+      let children = item.children;
+      nodes.push(item);
+      // 队列，先进先出
+      // nodes = [] stack = [parent]
+      // nodes = [parent] stack = [child1,child2,child3]
+      // nodes = [parent, child1] stack = [child2,child3,child1-1,child1-2]
+      // nodes = [parent,child1,child2]
+      for (let i = 0; i < children.length; i++) {
+        stack.push(children[i]);
+      }
+    }
+  }
+  return nodes;
+};
