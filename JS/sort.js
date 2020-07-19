cc0d6dacb83b68a25121f33063b79bcd393c3214
wushibao <wushibao@chengfayun.com>
2020-07-19 13:45:05 +0800

    .add select sort

--- JS/sort.js
+++ JS/sort.js
@@ -1,14 +1,37 @@
+// 交换位置
+const swap = (arr, i,j)=> {
+  let temp = arr[j];
+  arr[j] = arr [i];
+  arr[i] = temp;
+  return arr;
+}
+// 选择排序
+const selectSort = function(array) {
+  let minIndex;
+  for (i = 0; i < array.length; i++) {
+    minIndex = i;
+    for (let j= i + 1; j<array.length;j++) {
+      if (array[j] < array[minIndex]) {
+          minIndex = j;
+      }
+    }
+    swap(array, i, minIndex);
+  }
+  return array;
+}
+
+const testArray0 = [1, 6, 3, 7, 7, 2, 9];
+console.log(selectSort(testArray0));
+
 //冒泡
 const bubbleSort = function(array) {
   if (array.length <= 1) {
     return array;
   }
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (array[i] < array[j]) {
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
       }
     }
   }
